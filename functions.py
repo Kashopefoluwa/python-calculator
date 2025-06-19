5d21f7742cfbf68062d213329f401134c96dd094
Kashopefoluwa Agboola <kashopefoluwaa@gmail.com>
2025-06-19 17:35:10 -0400

    Add codes and functions to teach boolean

--- functions.py
+++ functions.py
@@ -23,12 +23,23 @@ def power(x, y):
 def upper_name (name):
     return name.upper() 
 
-user_name = input("Enter your name: ")
-uppercased_name = upper_name(user_name)
-greeting(uppercased_name)
+def celsius_to_fahrenheit(c):
+    return c * (9/5) + 32;
 
-def lower_name(name):
-    return name.lower()
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
 
-def length_of_name(name):
-    return len(name)
+answer = celsius_to_fahrenheit(100)
+print(answer)
+
+result = fahrenheit_to_celsius (100)
+print(result)
+# user_name = input("Enter your name: ")
+# uppercased_name = upper_name(user_name)
+# greeting(uppercased_name)
+
+# def lower_name(name):
+#     return name.lower()
+
+# def length_of_name(name):
+#     return len(name)
